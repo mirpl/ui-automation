27672e69f60c40e0f3d976cea3237a7778b16c89
daniel-mit <67262786+daniel-mit@users.noreply.github.com>
2020-09-17 22:15:16 +0200

    Update Service.java

--- src/test/java/ai/makeitright/tests/scraper/Service.java
+++ src/test/java/ai/makeitright/tests/scraper/Service.java
@@ -50,7 +50,7 @@ public class Service extends DriverConfig {
         int totalPages = getTotalPages(baseTargetURL);
         System.out.println("totalPages: " + totalPages);
         try {
-            Thread.sleep(100000);                 //1000 milliseconds is one second.
+            Thread.sleep(600000);                 //1000 milliseconds is one second.
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
