3b31351d7f3e33c016132fe0efb6685822fc7c8a
daniel-mit <67262786+daniel-mit@users.noreply.github.com>
2020-09-17 21:24:35 +0200

    Update Service.java

--- src/test/java/ai/makeitright/tests/scraper/Service.java
+++ src/test/java/ai/makeitright/tests/scraper/Service.java
@@ -23,6 +23,27 @@ import java.util.List;
 import java.util.Map;
 
 
+public class SleepMessages {
+    public static void main(String args[])
+        throws InterruptedException {
+        String importantInfo[] = {
+            "Mares eat oats",
+            "Does eat oats",
+            "Little lambs eat ivy",
+            "A kid will eat ivy too"
+        };
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(40000);
+            //Print a message
+            System.out.println(importantInfo[i]);
+        }
+    }
+}
+
 public class Service extends DriverConfig {
 
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
